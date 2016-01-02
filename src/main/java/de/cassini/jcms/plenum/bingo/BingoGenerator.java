ef7a9f59e80e317abb671c05696c9df3933bd9f1
Nils Hoffmann <nils.hoffmann@cassini.de>
2016-01-02 12:10:56 +0100

    skeletal main class implementation

---
+++ src/main/java/de/cassini/jcms/plenum/bingo/BingoGenerator.java
@@ -0,0 +1,50 @@
+package de.cassini.jcms.plenum.bingo;
+
+import java.io.File;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.converters.FileConverter;
+
+public class BingoGenerator {
+
+	// possible command line parameters
+	
+	@Parameter(
+			names={"-f", "--file"}, 
+			description="file with bingo values; if omitted, uses the built-in list", 
+			converter=FileConverter.class)
+	private File bingoValuesFile;
+	
+	@Parameter(
+			names={"-l", "--layout"},
+			description="file with the xslt/fop layout; if omitted, uses the built-in layout file",
+			converter=FileConverter.class
+			)
+	private File fopFile;
+	
+	@Parameter(
+			names={"-h", "--help"},
+			help=true)
+	private boolean help;
+	
+	public BingoGenerator() {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		BingoGenerator bingo = new BingoGenerator();
+		
+		JCommander cliParser = new JCommander(bingo, args);
+
+		
+		
+		if( bingo.help ) {
+			cliParser.usage();
+		}
+		
+		System.exit(0);
+	}
+}
---
+++ src/test/java/de/cassini/jcms/plenum/bingo/BingoGeneratorTest.java
@@ -0,0 +1,14 @@
+package de.cassini.jcms.plenum.bingo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BingoGeneratorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
